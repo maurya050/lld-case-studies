36c5f4504e14b25dbcbcd945689f78c27e33f8cf
maurya050 <mauryashivam050@gmail.com>
2026-08-08 11:30:47 +0530

    Singleton Implementaion

---
+++ Singleton/ThreadSafe_Eager_Singleton.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<mutex>
+
+using namespace std;
+
+class Singleton{
+
+    private:
+        static Singleton *instance;
+    
+        Singleton(){
+            cout<< "Singleton Constructor called !!!"<<endl;
+        }
+
+    public:
+        static Singleton* getInstance(){
+            return instance;
+        }
+};
+
+Singleton *Singleton::instance = new Singleton();
+
+int main(){
+    Singleton *s1 = Singleton::getInstance();
+    Singleton *s2 = Singleton::getInstance();
+
+    cout << (s1 == s2) <<endl;
+
+    return 0;
+}
